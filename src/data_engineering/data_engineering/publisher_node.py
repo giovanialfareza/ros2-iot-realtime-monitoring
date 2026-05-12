94392511b6420c926f10299abe440d772da81460
giovanialfareza <064002200045@std.trisakti.ac.id>
2026-05-12 04:55:29 +0000

    ROS2 IoT Monitoring Project

--- src/data_engineering/data_engineering/publisher_node.py
+++ src/data_engineering/data_engineering/publisher_node.py
@@ -2,38 +2,70 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
+import json
+from datetime import datetime
+
 
 class PublisherNode(Node):
+
     def __init__(self):
         super().__init__('publisher_node')
 
         # Publisher ke topic
         self.publisher_ = self.create_publisher(
             String,
-            '/sensor/data',
+            '/iot/data',
             10
         )
 
         # Timer publish tiap 1 detik
-        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.timer = self.create_timer(
+            1.0,
+            self.timer_callback
+        )
 
         self.count = 0
-        self.get_logger().info('Publisher node started')
+
+        self.get_logger().info(
+            'Publisher node started'
+        )
 
     def timer_callback(self):
+
+        # Payload simulasi sensor IoT
+        payload = {
+            "temperature": 29 + self.count,
+            "humidity": 70,
+            "ldr": 300,
+            "timestamp": datetime.now().isoformat()
+        }
+
+        # Convert JSON ke String ROS2
         msg = String()
-        msg.data = f'Data ke-{self.count}'
+        msg.data = json.dumps(payload)
+
+        # Publish data
         self.publisher_.publish(msg)
 
-        self.get_logger().info(f'Publish: "{msg.data}"')
+        # Logging terminal
+        self.get_logger().info(
+            f'Publish: "{msg.data}"'
+        )
+
         self.count += 1
 
 
 def main(args=None):
+
     rclpy.init(args=args)
 
     node = PublisherNode()
+
     rclpy.spin(node)
 
     node.destroy_node()
     rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
